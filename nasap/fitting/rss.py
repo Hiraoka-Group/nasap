4c8e95a11e4dbd2a40904d6bb446982d136f5e7b
neji-craftsman <142223934+neji-craftsman@users.noreply.github.com>
2024-11-28 21:05:52 +0900

    Enhance documentation for `calc_simulation_rss` function

--- nasap/fitting/rss.py
+++ nasap/fitting/rss.py
@@ -1,3 +1,6 @@
+"""Module for calculating the residual sum of squares (RSS) between the
+data and the simulation."""
+
 from collections.abc import Callable, Mapping
 from typing import Concatenate
 
@@ -11,6 +14,37 @@ def calc_simulation_rss(
             Concatenate[npt.NDArray, npt.NDArray, ...], npt.NDArray],
         y0: npt.NDArray, params_d: Mapping[str, float],
         ) -> float:
+    """Calculate the residual sum of squares (RSS) between the data 
+    and the simulation.
+
+    Parameters
+    ----------
+    tdata : npt.NDArray, shape (n,)
+        Time points of the data.
+    ydata : npt.NDArray, shape (n, m)
+        Data to be compared with the simulation.
+        If there are NaN values in the data, the corresponding rows
+        will be removed.
+    simulating_func : Callable
+        Function that simulates the system. 
+
+            ``simulating_func(t, y0, **params_d) -> y``
+
+        where ``t`` is a 1-D array with shape (n,), ``y0`` is a 1-D
+        array with shape (m,), ``params_d`` is a dictionary with the
+        parameters of the system, and ``y`` is a 2-D array with shape
+        (n, m).
+    y0 : npt.NDArray, shape (m,)
+        Initial conditions.
+    params_d : Mapping[str, float]
+        Parameters of the system.
+
+    Returns
+    -------
+    float
+        The residual sum of squares (RSS) between the data and the 
+        simulation.
+    """
     ysim = simulating_func(tdata, y0, **params_d)
 
     # Calculate the residuals
